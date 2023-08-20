79cd2bc950b4aabe98cc33663700bbf6ccb950b4
gyan-cell <jha07944@gmail.com>
2023-08-20 22:13:00 +0530

    Gonna Do backend From Start Again

--- pages/api/Login.ts
+++
@@ -1,7 +0,0 @@
-import { connectDb } from "@/utilities/connectDb";
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function (
-  req: NextApiRequest,
-  res: NextApiResponse,
-): Promise<void> {}
